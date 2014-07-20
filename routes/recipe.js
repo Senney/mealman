2ecf673b1502b85ec62461aa206bba692664a892
Sean Heintz <sean.heintz@gmail.com>
2014-07-20 03:09:32 -0600

    work on recipe routes

--- routes/recipe.js
+++ routes/recipe.js
@@ -5,6 +5,17 @@ router.get('/', function(req, res) {
     res.render('index', {title: 'recipe'});
 });
 
-router.get('/{id}')
+router.param('recipeid', /^\d+$/); // Recipes have an integer ID.
+router.get('/:recipeid', function(req, res) {
+    // TODO: Handle echoing a recipe back to the user here.
+});
+
+router.get('/add', function(req, res) {
+    // TODO: Serve the recipe-add page here.
+});
+
+router.post('/add', function(req, res) {
+    // TODO: Handle the addition of a recipe here.
+});
 
 module.exports = router;
